be57a26756bcf0a6ac529974252d32febd6bdaa6
Javier Serrano <Javier.Serrano@cern.ch>
2018-06-03 17:31:16 +0200

    More robust function to get an integer from the console

--- ch5/area.cpp
+++ ch5/area.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <string>
 #include <stdexcept>
+#include <limits>
 
 using namespace std;
+
+int get_an_int(string prompt){
+  int a;
+  while(true){
+    cout << prompt << ": ";
+    cin >> a;
+    if (cin) return a;
+     else {
+      cin.clear();
+      cout << "Sorry, I need an integer." << endl;
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+     }
+  }
+}
+
+
 int area(int length, int width){
 	if(length<0 || width <0)
 	  throw runtime_error("At least one argument is negative");
@@ -12,10 +30,8 @@ int main(){
 	int a;
 	int b;
 	cout << "Hello user" << endl;
-	cout << "Give me the width of the rectangle" << endl;
-	cin >> a;
-	cout << "Give me the length of the rectangle" << endl;
-	cin >> b;
+	a = get_an_int("Give me the width of the rectangle");
+	b = get_an_int("Give me the length of the rectangle");
 	try{
 	 int my_area = area(a,b);
 	 cout << "the area is " << my_area << "\n";
