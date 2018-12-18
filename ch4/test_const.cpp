60495dc7c72c47fbecc4aacd62668262bfd8fae2
a-sc <adrian.s.c.05@gmail.com>
2018-12-18 16:39:19 +0100

    test for constexpr

---
+++ ch4/test_const.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  constexpr double pi = 3.141592;
+  // long program
+  //pi = 3;
+  // rest of long program
+
+  cout << "The number pi is " << pi << endl;
+}
