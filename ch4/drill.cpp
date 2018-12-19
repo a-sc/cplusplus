b724c84096e5582c836821294f065764a7baddd7
a-sc <adrian.s.c.05@gmail.com>
2018-12-19 15:35:31 +0100

    Did steps 2 and 3 of the drill

--- ch4/drill.cpp
+++ ch4/drill.cpp
@@ -7,15 +7,33 @@ int main() {
   int val1;
   int val2;
   char c;
-  
+  int smaller;
+  int larger;
+
   while (true) {
     cout << "Enter two whole numbers, or | if you want to stop the program: ";
     cin >> val1 >> val2;
+    if (val1 >= val2) {
+      smaller = val2;
+      larger = val1;
+    }
+    else {
+      smaller = val1;
+      larger = val2;
+    }
+
     if(!cin){
       cin.clear();
       cin >> c;
       if(c=='|') return 0;
     } else
-    cout << "The numbers you entered are " << val1 << " and " << val2 << endl;
+
+    if (smaller != larger) {
+      cout << "The smaller number is " << smaller << " and the larger one is " <<
+       larger << endl;
+    }
+    else {
+      cout << "The numbers are equal" << endl;
+    }
   }
 }
